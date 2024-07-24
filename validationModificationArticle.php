3838a66e18c026d0987076cbfc04d3830b4dd6a6
charlesDmy <charles.damy@gmail.com>
2024-07-24 09:44:26 +0200

    modif

--- validationModificationArticle.php
+++ validationModificationArticle.php
@@ -4,7 +4,7 @@ $id=$_POST['id'];
 $nom_article= $_POST['nom_article'];
 $texte= $_POST['texte'];
 $photo= $_POST['photo'];
-print_r($_POST);
+// print_r($_POST);
 
 $modification=$bdd->prepare("UPDATE articles SET nom_article=:nom_article,texte=:texte,photo=:photo WHERE id =:id");
 $modification->bindParam(':nom_article',$nom_article, PDO::PARAM_STR);
